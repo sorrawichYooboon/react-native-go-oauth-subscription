f3bb725444eb2bf1962ebf54e6e73c6c0a966be6
Sorrawich Yooboon (Ko) <sorrawich.yoob@kkpfg.com>
2025-01-05 17:16:22 +0700

    chore:install require package to impl sign in

--- AuthConnect/ios/Pods/Target Support Files/ReactCodegen/ReactCodegen-umbrella.h
+++ AuthConnect/ios/Pods/Target Support Files/ReactCodegen/ReactCodegen-umbrella.h
@@ -13,6 +13,14 @@
 #import "FBReactNativeSpec/FBReactNativeSpec.h"
 #import "FBReactNativeSpecJSI.h"
 #import "RCTModulesConformingToProtocolsProvider.h"
+#import "react/renderer/components/RNGoogleSignInCGen/ComponentDescriptors.h"
+#import "react/renderer/components/RNGoogleSignInCGen/EventEmitters.h"
+#import "react/renderer/components/RNGoogleSignInCGen/Props.h"
+#import "react/renderer/components/RNGoogleSignInCGen/RCTComponentViewHelpers.h"
+#import "react/renderer/components/RNGoogleSignInCGen/ShadowNodes.h"
+#import "react/renderer/components/RNGoogleSignInCGen/States.h"
+#import "RNGoogleSignInCGen/RNGoogleSignInCGen.h"
+#import "RNGoogleSignInCGenJSI.h"
 
 FOUNDATION_EXPORT double ReactCodegenVersionNumber;
 FOUNDATION_EXPORT const unsigned char ReactCodegenVersionString[];
